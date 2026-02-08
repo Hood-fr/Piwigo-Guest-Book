33d9ea4720baa0769b422ecedc1713c392d01558
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-08 08:14:08 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%s says on %s :'] = '%s가 %s에 말했습니다:';
+$lang['* : mandatory fields'] = '*: 필수 입력란';
+$lang['Activate rating'] = '평점 활성화';
+$lang['Add link in menu'] = '메뉴에 링크 추가';
+$lang['GuestBook'] = '방명록';
+$lang['Guests (not registered) permissions :'] = '방문자(비회원) 권한:';
+$lang['Please enter a message'] = '메시지를 입력하세요';
+$lang['Please enter your e-mail'] = '이메일을 입력하세요';
+$lang['Please enter your username'] = '사용자 이름을 입력하세요';
+$lang['Rate'] = '평가하기';
+$lang['Send'] = '보내기';
+$lang['Sign the guestbook'] = '방명록에 서명하기';
+$lang['There are %d messages'] = '%d개의 메시지가 있습니다';
+$lang['Website'] = '웹사이트';
+$lang['add comment'] = '댓글 추가하기';
+$lang['invalid website address'] = '잘못된 웹사이트 주소입니다';
+$lang['not publicly visible'] = '공개적으로 표시되지 않습니다';
+$lang['view comments'] = '댓글 보기';
